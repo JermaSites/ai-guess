7e3370a6a8328cbeb7e9e9627bf9c447e13bf6d4
Thestickman391 <stickman391@gmail.com>
2021-02-08 13:35:54 +1300

    Setup serverside

--- scripts.js
+++ scripts.js
@@ -1,32 +1,43 @@
-const getRandomMessage = async () => {
-    const response = await fetch('https://jerma-ai.herokuapp.com/api/', {
+var message = document.getElementById("message");
+var displayMessage = document.getElementById("displayMessage");
+var correct = document.getElementById("result-correct");
+var incorrect = document.getElementById("result-incorrect");
+var streakIncorrect = document.getElementById("streak-incorrect");
+var streakCorrect = document.getElementById("streak-correct");
+var correctAnswer;
+var streak = 0;
+
+const newMessage = async () => {
+    const response = await fetch('http://localhost:5000/api/', {
         method: 'GET',
         headers: {
         'Content-Type': 'application/json'
         }
     });
     const myJson = await response.json(); //extract JSON from the http response
-    return myJson.string;
+    correctAnswer = myJson.result;
+    setTimeout(() => {
+        displayMessage.innerHTML = myJson.string;
+    }, 250);
     // document.getElementById("twitchChat").innerHTML = (twitchEmoji.parse( myJson.string ), { emojiSize : 'medium' } );
 };
-
-var message = document.getElementById("message");
-var correct = document.getElementById("result-correct");
-var incorrect = document.getElementById("result-incorrect");
 
 function guess (guess) {
     var result;
     console.log(guess);
-    if(guess === "ai") {
+    if(guess === correctAnswer) {
         result = correct;
     } else {
         result = incorrect;
     }
     result.style.transform = "scale(1)";
-    message.style.opacity = "0";
     message.style.pointerEvents = "none";
+    message.style.opacity = "0";
+    newMessage();
     result.style.transition = "transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
     if(result === correct) {
+        streak++;
+        streakCorrect.innerHTML = "Current Streak: " + streak;
         setTimeout(() => {
             result.style.transform = "scale(0)";
             message.style.opacity = "1";
@@ -35,10 +46,13 @@ function guess (guess) {
                 result.style.transition = "none";
             }, 200);
         }, 500);
+    } else {
+        streakIncorrect.innerHTML = "Streak: " + streak;
     }
 }
 
 function tryAgain () {
+    streak = 0;
     incorrect.style.transform = "scale(0)";
     message.style.opacity = "1";
     message.style.pointerEvents = "revert";
@@ -46,3 +60,8 @@ function tryAgain () {
         incorrect.style.transition = "none";
     }, 200);
 }
+
+newMessage();
+setTimeout(() => {
+    message.style.opacity = "1";
+}, 250);
---
+++ server/server.py
@@ -0,0 +1,29 @@
+import random 
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+aiLines = open("./ai.txt", encoding="utf8").readlines() 
+realLines = open("./real.txt", encoding="utf8").readlines() 
+
+@app.route('/api/', methods=['GET'])
+@cross_origin()
+def result():
+    rng = random.SystemRandom()
+    if(rng.random() > 0.5):
+        randomString = random.SystemRandom().choice(aiLines)
+        if randomString.strip() != "====================" and randomString.strip() != " " and randomString.strip() != "\n" and randomString.strip() != "@Jerma985" and randomString.strip() != ":":
+            print(randomString.strip())
+            return jsonify({'string': randomString.strip(), 'result': 'ai'})
+        else:
+            result()
+    else:
+        randomString = random.SystemRandom().choice(realLines)
+        return jsonify({'string': randomString.strip(), 'result': 'real'})
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
