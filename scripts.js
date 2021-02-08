c0ef0b2e3387675ad33e18f74c9d3c36029f8b0a
Thestickman391 <stickman391@gmail.com>
2021-02-08 13:10:32 +1300

    Finished guessing system

--- scripts.js
+++ scripts.js
@@ -12,19 +12,37 @@ const getRandomMessage = async () => {
 
 var message = document.getElementById("message");
 var correct = document.getElementById("result-correct");
+var incorrect = document.getElementById("result-incorrect");
 
 function guess (guess) {
+    var result;
     console.log(guess);
-    correct.style.transform = "scale(1)";
+    if(guess === "ai") {
+        result = correct;
+    } else {
+        result = incorrect;
+    }
+    result.style.transform = "scale(1)";
     message.style.opacity = "0";
     message.style.pointerEvents = "none";
-    correct.style.transition = "transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
-    setTimeout(() => {
-        correct.style.transform = "scale(0)";
-        message.style.opacity = "1";
-        message.style.pointerEvents = "revert";
+    result.style.transition = "transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+    if(result === correct) {
         setTimeout(() => {
-            correct.style.transition = "none";
-        }, 200);
-    }, 500);
+            result.style.transform = "scale(0)";
+            message.style.opacity = "1";
+            message.style.pointerEvents = "revert";
+            setTimeout(() => {
+                result.style.transition = "none";
+            }, 200);
+        }, 500);
+    }
+}
+
+function tryAgain () {
+    incorrect.style.transform = "scale(0)";
+    message.style.opacity = "1";
+    message.style.pointerEvents = "revert";
+    setTimeout(() => {
+        incorrect.style.transition = "none";
+    }, 200);
 }
